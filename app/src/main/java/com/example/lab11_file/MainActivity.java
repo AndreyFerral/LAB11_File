c45d06d9557e2ad00e3e8cc4846b8a467e0690ef
AndreyPC <tigra9900@mail.ru>
2021-10-03 18:54:04 +1000

    start made menu

--- app/src/main/java/com/example/lab11_file/MainActivity.java
+++ app/src/main/java/com/example/lab11_file/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lab11_file;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -21,6 +22,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 
     // Сохранение файла FileWriter
